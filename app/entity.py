0faa55a45d3b7abc5fd05f01b1be649f70e5ab86
Anastasia <a.s.baldina@mail.ru>
2025-12-11 23:52:56 +0300

    Revert "P10 fix false positive"
    This reverts commit 94719c9811e8b794b0d5c83bcb6fb3cfb0016eb9.

--- app/entity.py
+++ app/entity.py
@@ -3,8 +3,6 @@ from enum import Enum
 
 from pydantic import BaseModel
 
-from app.constants import STATUS_CLOSED, STATUS_IN_PROGRESS, STATUS_OPEN
-
 
 class Topic(BaseModel):
     title: str
@@ -13,6 +11,6 @@ class Topic(BaseModel):
 
 
 class TopicStatus(str, Enum):
-    open = STATUS_OPEN
-    in_progress = STATUS_IN_PROGRESS
-    closed = STATUS_CLOSED
+    open = "open"
+    in_progress = "in_progress"
+    closed = "closed"
